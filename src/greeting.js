15100caf161bf7799c75436435115c7c387bf64a
Andy Wendt <andy.wendt@sportsrecruits.com>
2023-07-18 12:32:16 -0600

    Final with evan

--- src/greeting.js
+++ src/greeting.js
@@ -1,4 +1,17 @@
+function createGreeting(name) {
+    let isShouted = name === name.toUpperCase()
 
+    let salutation = isShouted ? 'HELLO' : 'Hello,'
+    let endingPunctuation = isShouted ? '!' : '.'
 
+    return `${salutation} ${name}${endingPunctuation}`;
+}
 
-module.exports = function() {}
+module.exports = function(name) {
+    if (Array.isArray(name)) {
+        name = `${name[0]} and ${name[1]}`;
+    }
+
+    name = name ? name : 'my friend';
+    return createGreeting(name);
+}
--- src/greeting.test.js
+++ src/greeting.test.js
@@ -1,5 +1,21 @@
 const greeting = require('./greeting')
 
-test('foo', () => {
-    expect(true).toBe(true);
+test('simple greeting', () => {
+    expect(greeting('Bob')).toBe("Hello, Bob.");
+    expect(greeting('Amy')).toBe("Hello, Amy.");
 });
+test('stand-in greeting', () => {
+    expect(greeting(null)).toBe("Hello, my friend.");
+});
+
+test('shouting greeting', () => {
+    expect(greeting('JERRY')).toBe("HELLO JERRY!");
+});
+
+test('two name greeting', () => {
+    expect(greeting(["Jill", "Jane"])).toBe("Hello, Jill and Jane.");
+});
+
+// red / green / refactor
+// extract variable, extract function
+// inline variable, inline function
