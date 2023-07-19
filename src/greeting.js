7714de5183bff4af0757cbd0c0ba48029404c32d
Andy Wendt <andy.wendt@sportsrecruits.com>
2023-07-19 11:47:00 -0600

    final with everton

--- src/greeting.js
+++ src/greeting.js
@@ -1,4 +1,44 @@
 
 
 
-module.exports = function() {}
+module.exports = function(name) {
+    let names = buildName(name);
+    return buildSentence(names)
+}
+
+function buildName(name) {
+    if(!name) {
+        return 'my friend';
+    }
+
+    if(isMultipleNames(name)) {
+        return greetingMultipleNames(name);
+    }
+
+    return name;
+}
+
+function isMultipleNames(name) {
+    return Array.isArray(name)
+}
+
+function isShouted(names) {
+    return names === names.toUpperCase();
+}
+
+function buildSentence(names) {
+    if(isShouted(names)) {
+        return `HELLO ${names}!`;
+    }
+
+    return `Hello, ${names}.`;
+}
+
+function greetingMultipleNames(name) {
+    if(name.length > 2) {
+        let lastName = name.pop();
+        return  `${name.join(', ')}, and ${lastName}`;
+    }
+
+    return `${name.join(' and ')}`;
+}
--- src/greeting.test.js
+++ src/greeting.test.js
@@ -1,5 +1,24 @@
 const greeting = require('./greeting')
 
-test('foo', () => {
-    expect(true).toBe(true);
+test('simple greeting', () => {
+    expect(greeting("Bob")).toBe("Hello, Bob.");
+    expect(greeting("Mike")).toBe("Hello, Mike.");
 });
+
+test('Should be handle nulls by introducing a stand-in', () => {
+    expect(greeting(null)).toBe("Hello, my friend.");
+});
+
+test('Should be handle array', () => {
+    expect(greeting(["Jill", "Jane"])).toBe("Hello, Jill and Jane.");
+});
+
+test('Should be handle when shouting', () => {
+    expect(greeting("JERRY")).toBe("HELLO JERRY!");
+});
+
+test('Should be handle array with many names', () => {
+    expect(greeting(["Amy", "Brian", "Charlotte"])).toBe("Hello, Amy, Brian, and Charlotte.");
+});
+
+// red / green / refactor
